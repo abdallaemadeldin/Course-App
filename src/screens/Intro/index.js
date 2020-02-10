30761ad4218195980f3c67c51f07811f8ae958cc
abdallaemadeldin <abdallah.emad@entlaqa.com>
2020-02-10 16:32:31 +0200

    finish intro screen UI

---
+++ src/screens/Intro/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { View, Image, Text, TouchableOpacity } from 'react-native';
+import { styles } from './style';
+
+class Intro extends React.Component {
+    render() {
+        const { container, topCircle, rightCircle, logo, banner, introMsg, greenTxt, introDesc, basicBtn, btnTitle, greenBtn } = styles;
+
+        return (
+            <View style={container}>
+                {
+                    [...new Array(2)].map((e, i) => {
+                        return <View key={i} style={i === 0 ? topCircle : rightCircle} />
+                    })
+                }
+                <Image source={require('./../../assets/logo.png')} style={logo} />
+
+                <Image source={require('./../../assets/vector.png')} style={banner} />
+                <Text style={introMsg}>Welcome To<Text style={greenTxt}>{'\nReact Native Course'}</Text></Text>
+
+                <Text style={introDesc}>Today we will create chat application using react native</Text>
+
+                <TouchableOpacity style={[basicBtn, greenBtn]} activeOpacity={.8}>
+                    <Text style={btnTitle}>Login</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={basicBtn} activeOpacity={.8}>
+                    <Text style={btnTitle}>Sign Up</Text>
+                </TouchableOpacity>
+            </View>
+        );
+    }
+}
+
+export default Intro;
---
+++ src/screens/Intro/style.js
@@ -0,0 +1,88 @@
+import { Dimensions, StyleSheet, Image } from 'react-native';
+import { Colors } from './../../../app.json';
+
+const { width, height, fontScale } = Dimensions.get('screen');
+const logo = Image.resolveAssetSource(require('./../../assets/logo.png'));
+const logoWidth = width * 10 / 100;
+const logoHeight = (logo.height * logoWidth) / logo.width;
+const banner = Image.resolveAssetSource(require('./../../assets/vector.png'));
+const bannerWidth = width * 60 / 100;
+const bannerHeight = (banner.height * bannerWidth) / banner.width;
+const fontFamily = "Poppins-Bold";
+const fontFamilyR = "Poppins-Regular";
+
+export const styles = StyleSheet.create({
+    container: {
+        width: width,
+        height: height,
+        backgroundColor: Colors.white,
+        alignItems: 'center'
+    },
+    rightCircle: {
+        width: width * 60 / 100,
+        height: width * 60 / 100,
+        borderRadius: width * 30 / 100,
+        backgroundColor: Colors.dark,
+        opacity: .4,
+        position: 'absolute',
+        top: width * 20 / 100,
+        right: -width * 50 / 100
+    },
+    topCircle: {
+        width: width * 50 / 100,
+        height: width * 50 / 100,
+        borderRadius: width * 25 / 100,
+        backgroundColor: Colors.dark,
+        opacity: .4,
+        position: 'absolute',
+        top: -width * 30 / 100,
+        left: -width * 30 / 100
+    },
+    banner: {
+        width: bannerWidth,
+        height: bannerHeight,
+        marginTop: '12%'
+    },
+    logo: {
+        width: logoWidth,
+        height: logoHeight,
+        marginTop: '8%'
+    },
+    introMsg: {
+        fontFamily: fontFamily,
+        fontSize: 23 * fontScale,
+        textAlign: 'center',
+        color: Colors.dark,
+        marginTop: '8%'
+    },
+    greenTxt: {
+        fontSize: 20 * fontScale,
+        color: Colors.green
+    },
+    introDesc: {
+        width: '80%',
+        textAlign: 'center',
+        fontFamily: fontFamilyR,
+        fontSize: 15 * fontScale,
+        color: Colors.mintGray,
+        marginVertical: '5%'
+    },
+    basicBtn: {
+        width: '75%',
+        height: 44 * fontScale,
+        elevation: 5,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 21 * fontScale,
+        backgroundColor: Colors.dark,
+        marginBottom: height * 2.5 / 100
+    },
+    btnTitle: {
+        fontFamily: fontFamily,
+        fontSize: 14 * fontScale,
+        color: Colors.white
+    },
+    greenBtn: {
+        backgroundColor: Colors.green
+    }
+});

6a14120f16ed54be24f49631e9ac9a0605a64050
abdallaemadeldin <abdallah.emad@entlaqa.com>
2020-02-12 14:55:21 +0200

    add forgot password to stack

--- App.js
+++ App.js
@@ -13,6 +13,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 import Splash from './src/screens/Splash';
 import Intro from './src/screens/Intro';
 import SignIn from './src/screens/SignIn';
+import ForgetPassword from './src/screens/ForgetPassword';
 
 const Stack = createStackNavigator();
 
@@ -30,6 +31,7 @@ class App extends React.Component {
           <Stack.Screen name="Splash" component={Splash} />
           <Stack.Screen name="Intro" component={Intro} />
           <Stack.Screen name="SignIn" component={SignIn} />
+          <Stack.Screen name="ForgetPassword" component={ForgetPassword} />
         </Stack.Navigator>
       </NavigationContainer>
     );

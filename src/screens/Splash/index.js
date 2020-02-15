8dacb029498de94b6c3d3200cc8d0330b65e9bf7
Abdalla Emad Eldin <abdallah.emad@entlaqa.com>
2020-02-15 16:10:07 +0200

    check if user logged in in splash to navigate to home

--- src/screens/Splash/index.js
+++ src/screens/Splash/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import { View, Image } from 'react-native';
 import { styles } from './style';
+import auth from '@react-native-firebase/auth';
 
 class Splash extends React.Component {
 
     componentDidMount() {
+        let route = "Intro";
+        auth().onAuthStateChanged(user => {
+            if (user) {
+                route = "Home"
+            }
+        })
         setTimeout(() => {
-            this.props.navigation.replace('Intro');
+            this.props.navigation.replace(route);
         }, 2000);
     }
 

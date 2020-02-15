525f7e7ab58f3f0674d76ee1fd9b6db830a23a20
Abdalla Emad Eldin <abdallah.emad@entlaqa.com>
2020-02-15 16:08:54 +0200

    add home screen to stack and setup toolbar on home screen

--- App.js
+++ App.js
@@ -10,11 +10,14 @@ import React from 'react';
 import { StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import { Colors } from './app.json';
 import Splash from './src/screens/Splash';
 import Intro from './src/screens/Intro';
 import SignIn from './src/screens/SignIn';
 import SignUp from './src/screens/SignUp';
 import ForgotPassword from './src/screens/ForgotPassword';
+import Home from './src/screens/Home';
 
 const Stack = createStackNavigator();
 
@@ -23,17 +26,37 @@ class App extends React.Component {
     return (
       <NavigationContainer>
         <StatusBar hidden />
-        <Stack.Navigator headerMode="none" initialRouteName="Splash" mode="modal" keyboardHandlingEnabled={true} screenOptions={{
+        <Stack.Navigator headerMode="screen" initialRouteName="Splash" mode="modal" keyboardHandlingEnabled={true} screenOptions={{
           animationEnabled: true,
           animationTypeForReplace: "pop",
           cardOverlayEnabled: true,
           cardShadowEnabled: true
         }}>
-          <Stack.Screen name="Splash" component={Splash} />
-          <Stack.Screen name="Intro" component={Intro} />
-          <Stack.Screen name="SignIn" component={SignIn} />
-          <Stack.Screen name="SignUp" component={SignUp} />
-          <Stack.Screen name="ForgotPassword" component={ForgotPassword} />
+          <Stack.Screen name="Splash" component={Splash} options={{
+            headerShown: false
+          }} />
+          <Stack.Screen name="Intro" component={Intro} options={{
+            headerShown: false
+          }} />
+          <Stack.Screen name="SignIn" component={SignIn} options={{
+            headerShown: false
+          }} />
+          <Stack.Screen name="SignUp" component={SignUp} options={{
+            headerShown: false
+          }} />
+          <Stack.Screen name="ForgotPassword" component={ForgotPassword} options={{
+            headerShown: false
+          }} />
+          <Stack.Screen name="Home" component={Home} options={{
+            headerStyle: {
+              backgroundColor: Colors.green,
+              borderBottomLeftRadius: 20,
+              borderBottomRightRadius: 20,
+              elevation: 0,
+              shadowOpacity: 0
+            },
+            headerTintColor: Colors.white
+          }} />
         </Stack.Navigator>
       </NavigationContainer>
     );
